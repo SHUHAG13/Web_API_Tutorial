e2e2db48ca6534dab645337199d1a72808571776
SHUHAG13 <shuhag200113@gmail.com>
2026-03-29 22:11:16 +0600

    Add project files.

---
+++ Controllers/BooksController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Web_API_Tutorial.Data;
+
+namespace Web_API_Tutorial.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BooksController:ControllerBase
+    {
+            private readonly AppDbContext _context;
+    
+            public BooksController(AppDbContext context)
+            {
+                _context = context;
+            }
+
+        [HttpGet("getById")]
+        public async Task<IActionResult> GetAvailableBooks(int id)
+        {
+            var books = await _context.Books.FirstOrDefaultAsync(b => b.Id==id);
+            return Ok(books);
+        }
+
+        [HttpGet("allAvailableBooks")]
+            public async Task<IActionResult> GetAvailableBooks()
+            {
+                var books = await _context.Books.Where(b => b.IsAvailable).ToListAsync();
+                return Ok(books);
+            }
+
+          [HttpGet("filterByPrice")]
+          public async Task<IActionResult>FilterByPrice()
+           {
+                var filteredBooks = await _context.Books.Where(b => b.IsAvailable && b.Price > 700).ToListAsync();
+                return Ok(filteredBooks);
+
+           }
+
+        [HttpGet("orderByPrice")]
+        public async Task<IActionResult> OrderByPrice()
+        {
+            var filteredBooks = await _context.Books.OrderByDescending(b=>b.Price).ToListAsync();
+            return Ok(filteredBooks);
+
+        }
+
+        [HttpGet("returnSpecificData")]
+        public async Task<IActionResult> ReturnSpecificData()
+        {
+            var books = await _context.Books.Select(b => new
+            {
+                b.Price,
+                b.Author
+            }).ToListAsync();
+            return Ok(books);
+
+        }
+
+
+    }
+}
---
+++ Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Web_API_Tutorial.Models;
+
+namespace Web_API_Tutorial.Data
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Book> Books { get; set; }
+    }
+}
