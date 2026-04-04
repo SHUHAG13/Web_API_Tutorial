5f9d5ddb54fc807862354df0cde0379f92a5de1a
SHUHAG13 <shuhag200113@gmail.com>
2026-04-04 23:31:35 +0600

    Implement GroupBy and aggregation in GroupOperatorController: - Author-wise book summary using Count, Sum, Average, Max, Min - Added BookSummaryDto for clean response - Added filtered summary for available books

---
+++ Controllers/GroupOperatorController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Web_API_Tutorial.Data;
+using Web_API_Tutorial.Models;
+
+namespace Web_API_Tutorial.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GroupOperatorController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public GroupOperatorController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("groupBy")]
+        public async Task<IActionResult>GetBooksSummery()
+        {
+            var data = await _context.Books
+                .GroupBy(b => b.Author.Name)
+                .Select(g => new BookSummaryDto
+                {
+                    AuthorName = g.Key,
+                    TotalBooks = g.Count(),
+                    TotalPrice = g.Sum(x => x.Price),
+                    AvgPrice = g.Average(x => x.Price),
+                    MaxPrice = g.Max(x => x.Price),
+                    MinPrice = g.Min(x => x.Price)
+
+                }).ToListAsync();
+            return Ok(data);
+        }
+
+        [HttpGet("availableSummery")]
+        public async Task<IActionResult>GetAvailableBooksSummery()
+        {
+            var data = await _context.Books
+                .Where(b => b.IsAvailable)
+                .GroupBy(b => b.Author.Name)
+                .Select(g => new
+                {
+                    AuthorName = g.Key,
+                    TotalBooks = g.Count(),
+                    AvgPrice = g.Average(x => x.Price)
+
+                }).ToListAsync();
+
+            return Ok(data);
+        }
+    }
+}
---
+++ Models/BookSummaryDto.cs
@@ -0,0 +1,12 @@
+﻿namespace Web_API_Tutorial.Models
+{
+    public class BookSummaryDto
+    {
+        public string AuthorName { get; set; }
+        public int TotalBooks { get; set; }
+        public decimal TotalPrice { get; set; }
+        public decimal AvgPrice { get; set; }
+        public decimal MaxPrice { get; set; }
+        public decimal MinPrice { get; set; }
+    }
+}
